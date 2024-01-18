e6bb76d2af0bee4a24660a2b70f8126815bbba30
Anthony-19 <116091134+Anthony-19@users.noreply.github.com>
2024-01-18 11:55:10 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,11 @@ import "./style.css";
 
 export default function App() {
   const [track, setTrack] = React.useState(0);
-//   const counterMinus = track - 1;
-//   const counterPlus = track + 1;
-//   function minus() {
-//     setTrack(counterMinus);
-//   }
-//   function plus() {
-//     setTrack(counterPlus);
-//   }
-//   console.log(track);
 function minus() {
     setTrack(function(){
         return track - 1
     })
 }
-// function plus() {
-//     setTrack(() => track + 1)
-// }
 function plus() {
     setTrack(prevTrack => prevTrack + 1)
 }
